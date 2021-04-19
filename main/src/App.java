764b1190d76a0e6fe91fced94225a3faf97ad00d
Kohei Yoshikawa <marimo3418@neko2.net>
2021-04-19 09:10:04 +0000

    added while loop on main function

--- main/src/App.java
+++ main/src/App.java
@@ -7,22 +7,26 @@ public class App {
         Point2D.Double point_test = new Point2D.Double();
         Random rand = new Random();
         int dist;
+        int count = 0;
 
-        point_test.setLocation();
-        dist = rand.nextInt(100);
+        point_test.setLocation(0, 0);
 
-        switch(rand.nextInt(3)){
-            case 0:     Move.positive_x(point_test, dist);
-                        break;
-            case 1:     Move.negative_x(point_test, dist);
-                        break;
-            case 2:     Move.positive_y(point_test, dist);
-                        break;
-            case 3:     Move.negative_y(point_test, dist);
-                        break;
-            default:    break;
+        while(count<100){
+            dist = rand.nextInt(100);
+
+            switch(rand.nextInt(4)){
+                case 0:     Move.positive_x(point_test, dist);
+                            break;
+                case 1:     Move.negative_x(point_test, dist);
+                            break;
+                case 2:     Move.positive_y(point_test, dist);
+                            break;
+                case 3:     Move.negative_y(point_test, dist);
+                            break;
+                default:    break;
+            }
+            count += 1;
         }
-
         System.out.println(point_test.toString());
     }
 }
--- main/src/Move.java
+++ main/src/Move.java
@@ -3,18 +3,18 @@ import java.awt.geom.Point2D;
 public class Move {
   static void negative_x(Point2D.Double point, int x){
     point.setLocation(point.x-x, point.y);
-    System.out.println("Done.");
+    //System.out.println("Done.");
   }
   static void positive_x(Point2D.Double point, int x){
     point.setLocation(point.x+x, point.y);
-    System.out.println("Done.");
+    //System.out.println("Done.");
   }
   static void positive_y(Point2D.Double point, int y){
     point.setLocation(point.x, point.y+y);
-    System.out.println("Done.");
+    //System.out.println("Done.");
   }
   static void negative_y(Point2D.Double point, int y){
     point.setLocation(point.x, point.y-y);
-    System.out.println("Done.");
+    //System.out.println("Done.");
   }
 }
