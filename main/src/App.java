4ed716fc53d2cc60d8172363ee917f477946f5ad
Kohei Yoshikawa <marimo3418@neko2.net>
2021-04-26 05:30:33 +0000

    Added random_walk function to Move.java

--- main/src/App.java
+++ main/src/App.java
@@ -1,32 +1,14 @@
-import java.lang.Object;
 import java.util.Random;
 import java.awt.geom.Point2D;
 
 public class App {
+    static int time_sec = 10;
     public static void main(String[] args) throws Exception {
         Point2D.Double point_test = new Point2D.Double();
         Random rand = new Random();
-        int dist;
-        int count = 0;
 
         point_test.setLocation(0, 0);
 
-        while(count<10){
-            dist = rand.nextInt(100);
-
-            switch(rand.nextInt(4)){
-                case 0:     Move.positive_x(point_test, dist);
-                            break;
-                case 1:     Move.negative_x(point_test, dist);
-                            break;
-                case 2:     Move.positive_y(point_test, dist);
-                            break;
-                case 3:     Move.negative_y(point_test, dist);
-                            break;
-                default:    break;
-            }
-            System.out.println("point_test (" + point_test.x + ", " + point_test.y + ")");
-            count += 1;
-        }
+        Move.random_walk(point_test, rand, time_sec);   
     }
 }
--- main/src/Move.java
+++ main/src/Move.java
@@ -1,4 +1,5 @@
 import java.awt.geom.Point2D;
+import java.util.Random;
 
 public class Move {
   static void negative_x(Point2D.Double point, int x){
@@ -16,5 +17,25 @@ public class Move {
   static void negative_y(Point2D.Double point, int y){
     point.setLocation(point.x, point.y-y);
     //System.out.println("Done.");
+  }
+  static void random_walk(Point2D.Double point, Random rand, int time_sec){
+    int count = 0;
+    while(count<time_sec){
+      int dist = rand.nextInt(100);
+
+      switch(rand.nextInt(4)){
+          case 0:     Move.positive_x(point, dist);
+                      break;
+          case 1:     Move.negative_x(point, dist);
+                      break;
+          case 2:     Move.positive_y(point, dist);
+                      break;
+          case 3:     Move.negative_y(point, dist);
+                      break;
+          default:    break;
+      }
+      System.out.println("point (" + point.x + ", " + point.y + ")");
+      count += 1;
+  }
   }
 }
