434a468471489168b7b4944362f24ed18b3c63ca
Kohei Yoshikawa <marimo3418@neko2.net>
2021-05-06 11:08:12 +0000

    Fixed Node counter

--- main/src/App.java
+++ main/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+
 import java.awt.geom.Point2D;
 
 class Node_info{
@@ -12,15 +13,19 @@ public class App {
     static int edge_dist = 2000;
     public static void main(String[] args) throws Exception {
         Random rand = new Random();
+        //List<Node_info> node_list = new ArrayList<>();
         Node_info[] node_array = new Node_info[2];
+        int node_leased = 0;
 
         for(int i=0; i<node_array.length; i++) node_array[i] = new Node_info();
 
-        Node_mng.init(rand, node_array[0], 1000, 1000);
+        node_leased = Node_mng.init(rand, node_leased, node_array[0], 1000, 1000);
+        node_leased = Node_mng.init(rand, node_leased, node_array[1], 1000, 1000);
 
         int count = 0;
         while(count < time_sec){
-            Move.random_walk(node_array[0], rand); 
+            Move.random_walk(node_array[0], rand);
+            Move.random_walk(node_array[1], rand);
             count += 1;
         }  
     }
--- main/src/Node_mng.java
+++ main/src/Node_mng.java
@@ -4,10 +4,11 @@ import java.lang.ProcessHandle.Info;
 import java.util.Random;
 
 public class Node_mng {
-    static void init(Random rand, Node_info node, int init_x, int init_y){
+    static int init(Random rand, int node_leased, Node_info node, int init_x, int init_y){
         //Initialize Node. Set num, first location, move speed.
-        node.num = 1;
+        node.num = node_leased + 1;
         node.point.setLocation(init_x, init_y);
         node.move_speed = rand.nextInt(40)+10;
+        return node_leased + 1;
     }
 }
