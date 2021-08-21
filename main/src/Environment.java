eaad762814262ff220325f6c64ed7b41a7657824
marim01110 <marimo3418@neko2.net>
2021-08-21 08:26:14 +0000

    Replace with return_move_speed function

--- main/src/Environment.java
+++ main/src/Environment.java
@@ -6,13 +6,26 @@ public class Environment {
     private static int[] landmark_point_y_array = {1000, 200, 500};
   
     //------NODE MOVE SPEED SETTING------
-    private static final int MOVE_SPEEDS = 0;
-    private static int[] move_speed_array = {};
+    static final int MOVE_SPEEDS = 4;
+    private static int[] move_speed_array = {55, 80, 250, 330};
+
+    /*
+    http://www.yspc-ysmc.jp/ysmc/column/health-fitness/walking-2.html
+    https://bike.shimano.com/ja-jp/mindswitch/lab/89/#:~:text=%E3%82%B7%E3%83%86%E3%82%A3%E3%82%B5%E3%82%A4%E3%82%AF%E3%83%AB%EF%BC%88%E3%81%84%E3%82%8F%E3%82%86%E3%82%8B%E3%83%9E%E3%83%9E%E3%83%81%E3%83%A3%E3%83%AA%EF%BC%89%E3%81%AE,%E6%98%8E%E3%82%89%E3%81%8B%E3%81%AB%E3%81%AA%E3%81%A3%E3%81%A6%E3%81%84%E3%81%BE%E3%81%99%E3%80%82
+    https://www.ktr.mlit.go.jp/gaikan/pi_kouhou/jigyou_gaiyou2015/p6-7.pdf
+    */
 
     static Point2D.Double return_landmark_point(int index){
       var point = new Point2D.Double();
 
       point.setLocation(landmark_point_x_array[index], landmark_point_y_array[index]);
       return point;
+    }
+
+    static int return_move_speed(int index){
+      int move_speed;
+
+      move_speed = move_speed_array[index];
+      return move_speed;
     }
 }
--- main/src/Node_mng.java
+++ main/src/Node_mng.java
@@ -5,24 +5,21 @@ import java.util.ArrayList;
 public class Node_mng {
   private static final boolean DEBUG = App.DEBUG;
 
-  //------NODE MOVE SPEED SETTING------
-  private static final int MOVE_SPEEDS = 0;
-  private static int[] move_speed_array = {};
-
   static Node_info generate(ArrayList<Node_info> node_list, int node_leased){
     Random rand = new Random();
     Point2D.Double start = new Point2D.Double();
     Point2D.Double destination = new Point2D.Double();
-    int point_index, destination_index, move_speed;
+    int point_index, destination_index, move_speed, move_speed_index;
 
     do{
       point_index = rand.nextInt(Environment.LANDMARKS);
       destination_index = rand.nextInt(Environment.LANDMARKS);
     }while(point_index == destination_index);
+    move_speed_index = rand.nextInt(Environment.MOVE_SPEEDS);
 
     start.setLocation(Environment.return_landmark_point(point_index));
     destination.setLocation(Environment.return_landmark_point(destination_index));
-    move_speed = rand.nextInt(40) + 10;
+    move_speed = Environment.return_move_speed(move_speed_index);
 
     var newnode = new Node_info(node_leased, start, destination, false, false, false, move_speed);
     if(DEBUG) System.out.println("Node " + newnode.num + " Created. Start from " + newnode.point + ", Goal is " + newnode.destination);
